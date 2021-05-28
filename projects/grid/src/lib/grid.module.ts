8d05b7f41122cbf4f402b4a931f59a3814b367b7
Denis Lubchenko <rexar1988@gmail.com>
2021-05-28 20:28:13 +0300

    feature: add grid module

---
+++ projects/grid/src/lib/grid.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { GridComponent } from './grid.component';
+
+
+
+@NgModule({
+  declarations: [
+    GridComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    GridComponent
+  ]
+})
+export class GridModule { }
